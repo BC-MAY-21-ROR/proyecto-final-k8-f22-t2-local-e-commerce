c78b9ef8a13d0e79278fa260215d04c477e13bcb
juperameza <juperameza@hotmail.com>
2022-04-12 18:43:22 -0500

    Change the name data type on Type table
    Changed the name from string to boolean on the Type table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_12_224549) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_12_233742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -94,7 +94,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_12_224549) do
   end
 
   create_table "types", force: :cascade do |t|
-    t.string "name"
+    t.boolean "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
