ac3198262d716fd2e385a203842fcb35761cc721
juperameza <juperameza@hotmail.com>
2022-04-20 14:26:42 -0500

    Generate migration for user comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_12_233742) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_20_192429) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -110,6 +110,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_12_233742) do
     t.integer "ranking"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_user_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -138,5 +140,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_12_233742) do
   add_foreign_key "posts", "types"
   add_foreign_key "reservations", "posts"
   add_foreign_key "schedules", "posts"
+  add_foreign_key "user_comments", "users"
   add_foreign_key "users", "statuses"
 end
