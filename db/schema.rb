8a7a11aa06c5491318be15b589a2a6ae7164e6e6
juperameza <juperameza@hotmail.com>
2022-04-21 15:36:50 -0500

    Add default value to status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_20_192758) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_21_203558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,7 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_20_192758) do
     t.bigint "type_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "status_id", null: false
+    t.bigint "status_id", default: 1, null: false
     t.index ["status_id"], name: "index_posts_on_status_id"
     t.index ["type_id"], name: "index_posts_on_type_id"
   end
