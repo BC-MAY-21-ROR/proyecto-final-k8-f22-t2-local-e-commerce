dd64dd57d3f961a19debfcdc8c6ea6e55cc3748e
juperameza <juperameza@hotmail.com>
2022-04-21 15:16:32 -0500

    Change the scaffold defaults to render post and use simple form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.fetch(:post, {})
+      params.fetch(:post, {}).permit(:title,:description,:picutre,:price,:type_id,:status_id)
     end
 end
