a466f5be46d89453737cb79a61839cb2bbf626f8
Javi1499 <fruiz1@ucol.mx>
2022-05-03 18:36:05 -0500

    Changes in order migration done

--- db/schema.rb
+++ db/schema.rb
@@ -71,8 +71,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_215121) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "total"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "post_categories", force: :cascade do |t|
