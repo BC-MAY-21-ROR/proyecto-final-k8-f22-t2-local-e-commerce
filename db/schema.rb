51211e5745197c3f33a06d28aaf1bdd829941562
juperameza <juperameza@hotmail.com>
2022-05-05 15:45:02 -0500

    Commiting to rebase master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_04_27_215121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,7 +109,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_215121) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "status_id", default: 1, null: false
-    t.index ["status_id"], name: "index_posts_on_status_id"
+    t.index [" "], name: "index_posts_on_status_id"
     t.index ["type_id"], name: "index_posts_on_type_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
