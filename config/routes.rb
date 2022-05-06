c721e22f3b0660af39be6d616aa7dc0a30c948c4
EliasUR <eliasrodriguez2002@hotmail.com>
2022-05-06 20:55:15 -0300

    Add routes to buy a product

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  resources :post_comments, only: [:create]
-  resources :posts
   devise_for :users
+  resources :posts
+  resources :post_comments, only: [:create]
+  resources :order, only: [:index, :show]
   resources :users, only: [:show] do
     collection do
       get :profile
     end
   end
   get 'post/:id/new_comment', to: 'post_comments#new', as: 'new_comment'
+  post 'post/:id/buy_product', to: 'order#buy', as: 'buy_product' 
   root to: 'home#index'
 end
