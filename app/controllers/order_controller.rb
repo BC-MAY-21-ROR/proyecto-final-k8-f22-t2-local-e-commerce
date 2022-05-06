d393f166c08da4f05066118e8d4fe1b17eec3630
EliasUR <eliasrodriguez2002@hotmail.com>
2022-05-06 20:53:00 -0300

    Add controller and methods to buy a product

---
+++ app/controllers/order_controller.rb
@@ -0,0 +1,25 @@
+class OrderController < ApplicationController
+
+	def index
+		@orders = OrderDetail.all.order('created_at DESC')#Hay que modificarlo con user.id
+		render 'orders/index'
+	end
+
+	def buy
+		@order_details = OrderDetail.buy(order_details_params)
+		respond_to do |format|
+				if @order_details.save
+					format.html { redirect_to action: "index", notice: "Compra realizada con exito" }
+					format.json { render :index, status: :created, location:order_details }
+				else
+					format.html { redirect_to post_url(@order_details.post), status: :unprocessable_entity }
+					format.json { render json: @order_details.errors, status: :unprocessable_entity }
+				end
+		  end
+	end
+
+	private
+		def order_details_params
+			params.require(:order_details).permit(:quantity, :post_id, :price)
+		end
+end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,11 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :post
+  def self.buy(params)
+    total = params[:price].to_i*params[:quantity].to_i
+    order = Order.create(total:total)
+    order_detail = create(price:params[:price], quantity:params[:quantity], 
+                          post_id:params[:post_id], order_id:order.id)
+    order_detail
+  end
 end
