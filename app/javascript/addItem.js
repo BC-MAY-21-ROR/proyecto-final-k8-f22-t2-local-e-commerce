607b2e481ebe7f5c40847b6b6c2fcd97d2629863
German Ruiz <germanruzca@gmail.com>
2022-05-12 17:26:21 -0500

    Change some styles and indentation

--- app/javascript/addItem.js
+++ app/javascript/addItem.js
@@ -17,23 +17,6 @@ const showImages = (images) => {
     img = document.getElementsByClassName('output');
     img[i].src = URL.createObjectURL(images[i]);
     img[i].style.display = 'inline';
-	const pictures = event.target.files;
-  
-  for(let i=0; i<pictures.length; i++) {
-    images.push(pictures[i]);
-	}
-	
-  showImages(images);
-}
-
-const showImages = (images) => {
-  container.innerHTML = ''
-  for (let i = 0; i < images.length; i++) {
-    let img = `<div class="form_new_post__container"><img class="form_new_post__picture output"/><div onclick="deleteImage(${i})" class="form_new_post__delete"><a class="form_new_post__delete__icon"><i class="fa-solid fa-trash"></i></a></div></div>`;
-    container.innerHTML += img;
-    img = document.getElementsByClassName('output');
-    img[i].src = URL.createObjectURL(images[i]);
-    img[i].style.display = 'inline';
 	}
 }
 
