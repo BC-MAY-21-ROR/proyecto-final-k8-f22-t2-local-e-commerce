da730192c686c0cec50b94efe5e05103a3375c87
German Ruiz <germanruzca@gmail.com>
2022-05-12 16:16:53 -0500

    Update addItem.js

--- app/javascript/addItem.js
+++ app/javascript/addItem.js
@@ -2,36 +2,36 @@ let images = [];
 let container = document.getElementById("upload");
 
 window.loadFiles = function(event) {
-		const pictures = event.target.files;
-
-		for(let i=0; i<pictures.length; i++) {
-			images.push(pictures[i]);
-		}
-
-		showImages(images);
+	const pictures = event.target.files;
+	
+	for(let i=0; i<pictures.length; i++) {
+		images.push(pictures[i]);
+	}
+	
+	showImages(images);
 }
 
 const showImages = (images) => {
-		container.innerHTML = ''
-		for (let i = 0; i < images.length; i++) {
-				let img = `<div class="form_new_post__container"><img class="form_new_post__picture output"/><div onclick="deleteImage(${i})" class="form_new_post__delete"><a class="form_new_post__delete__icon"><i class="fa-solid fa-trash"></i></a></div></div>`;
-				container.innerHTML += img;
-				img = document.getElementsByClassName('output');
-				img[i].src = URL.createObjectURL(images[i]);
-				img[i].style.display = 'inline';
-		}
+	container.innerHTML = ''
+	for (let i = 0; i < images.length; i++) {
+		let img = `<div class="form_new_post__container"><img class="form_new_post__picture output"/><div onclick="deleteImage(${i})" class="form_new_post__delete"><a class="form_new_post__delete__icon"><i class="fa-solid fa-trash"></i></a></div></div>`;
+		container.innerHTML += img;
+		img = document.getElementsByClassName('output');
+		img[i].src = URL.createObjectURL(images[i]);
+		img[i].style.display = 'inline';
+	}
 }
 
 const removeChilds = (imgs) => {
-		imgs = document.getElementsByClassName('output');
-  let parent = imgs[0].parentNode;
-		let size = imgs.length;
-		for (let key = 0; key <= size; key++) {
-				parent.removeChild(parent.lastChild);
-		}
+	imgs = document.getElementsByClassName('output');
+	let parent = imgs[0].parentNode;
+	let size = imgs.length;
+	for (let key = 0; key <= size; key++) {
+		parent.removeChild(parent.lastChild);
+	}
 }
 
 window.deleteImage = function(position) {
-		images.splice(position, 1);
-		showImages(images);
+	images.splice(position, 1);
+	showImages(images);
 }
