81dc10b84d7dfca0dd7024a86ad1920ed207a191
EliasUR <eliasrodriguez2002@hotmail.com>
2022-05-13 19:32:08 -0300

    change import of addItem from 'application.html.erb' to 'application.js'

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,7 @@ Rails.application.routes.draw do
     end
   end
   get 'post/:id/new_comment', to: 'post_comments#new', as: 'new_comment'
-<<<<<<< HEAD
   post 'post/:id/buy_product', to: 'order#buy', as: 'buy_product' 
-=======
   get 'user/sold', to: 'users#sold', as: 'sold'
->>>>>>> e1fd728 (Add files to show user sold products)
   root to: 'home#index'
 end
