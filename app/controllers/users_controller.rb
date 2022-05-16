997a1e91b8aca01e8dc64f261f782990b60d0f23
German Ruiz <germanruzca@gmail.com>
2022-05-16 11:27:00 -0500

    Added the views, method and controller of favorites page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:profile]
+  before_action :authenticate_user!, only: [:profile, :favorites]
   def show
     @user = User.find(params[:id])
   end
 
   def profile
     @user = current_user
-  end
+	end
+
+	def	favorites
+		@user = current_user
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show] do
     collection do
       get :profile
+			get :favorites
     end
   end
   get 'post/:id/new_comment', to: 'post_comments#new', as: 'new_comment'
