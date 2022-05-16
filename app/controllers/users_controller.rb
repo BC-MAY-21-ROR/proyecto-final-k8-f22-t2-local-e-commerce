7e330deabebbfee214cca798813367ef6e68a971
German Ruiz <germanruzca@gmail.com>
2022-05-16 18:14:33 -0500

    Add the message method to before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:profile, :favorites, :notifications]
+  before_action :authenticate_user!, only: [:profile, :favorites, :notifications, :messages]
   def show
     @user = User.find(params[:id])
   end
