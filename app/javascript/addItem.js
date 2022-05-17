246f8239e091e76a5642df84b1f0abaeb0ebf7b9
German Ruiz <germanruzca@gmail.com>
2022-05-17 13:53:37 -0500

    tests

--- app/javascript/addItem.js
+++ app/javascript/addItem.js
@@ -1,6 +1,7 @@
 let images = [];
 let container = document.getElementById("upload");
 
+
 window.loadFiles = function(event) {
   const pictures = event.target.files;
   for(let i=0; i<pictures.length; i++) {
@@ -10,7 +11,11 @@ window.loadFiles = function(event) {
 }
 
 const showImages = (images) => {
-  container.innerHTML = ''
+  // container.innerHTML = ''
+  console.log(container)
+  // for(let i=0; i<hola.length; i++) {
+  //   container.innerHTML += `<h1>Gilk</h1>`
+  // }
   for (let i = 0; i < images.length; i++) {
     let img = `<div class="form_new_post__container"><img class="form_new_post__picture output"/><div onclick="deleteImage(${i})" class="form_new_post__delete"><a class="form_new_post__delete__icon"><i class="fa-solid fa-trash"></i></a></div></div>`;
     container.innerHTML += img;
@@ -18,6 +23,8 @@ const showImages = (images) => {
     img[i].src = URL.createObjectURL(images[i]);
     img[i].style.display = 'inline';
   }
+
+  console.log(container);
 }
 
 window.deleteImage = function(position) {
