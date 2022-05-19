649cea5aacfd14d9c21249d71144ed7d6e0325d8
German Ruiz <germanruzca@gmail.com>
2022-05-19 14:19:46 -0500

    Add the messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,6 @@ class PostsController < ApplicationController
 
   # GET /posts/1 or /posts/1.json
   def show
-    @comments_average =  @post.post_comments.average(:ranking)
   end
 
   # GET /posts/new
@@ -22,7 +21,7 @@ class PostsController < ApplicationController
     @post = current_user.posts.find_by(id: params[:id])
 
     unless @post
-      redirect_to root_path, alert: 'You can only edit your own posts'
+      redirect_to root_path, alert: 'Ups! No puedes editar este producto'
     end
   end
 
@@ -31,7 +30,7 @@ class PostsController < ApplicationController
     @post = current_user.posts.new(post_params)
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.html { redirect_to post_url(@post), notice: "Producto creado exitosamente." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -49,7 +48,7 @@ class PostsController < ApplicationController
     end
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
+        format.html { redirect_to post_url(@post), notice: "Producto editado exitosamente." }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -64,7 +63,7 @@ class PostsController < ApplicationController
     @post.destroy
 
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.html { redirect_to posts_url, notice: "Producto eliminado exitosamente." }
       format.json { head :no_content }
     end
   end
