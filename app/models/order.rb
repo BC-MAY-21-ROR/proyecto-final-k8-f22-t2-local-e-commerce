4d7728cbd71d17ca50397bffa9821f75958251e5
EliasUR <eliasrodriguez2002@hotmail.com>
2022-05-25 18:44:16 -0300

    Add method to access buyer user from orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,9 @@
 class Order < ApplicationRecord
-    has_many :order_details
-    has_many :posts, through: :order_details
-    has_one :user
+	has_many :order_details
+	has_many :posts, through: :order_details
+	has_one :user
+
+	def user
+		user = User.find(self.user_id)	
+	end
 end
