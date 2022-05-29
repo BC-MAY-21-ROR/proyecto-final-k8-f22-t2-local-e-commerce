009dafa2134cb69d2b1177b2a92c92a3620a822c
juperameza <juperameza@hotmail.com>
2022-05-28 21:04:53 -0500

    Left to do
    Bulk action to buy cart
    View like mercado libre when adding product to cart
    styles maybe
    Dropdown act

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,12 +3,9 @@ class Cart < ApplicationRecord
   has_and_belongs_to_many :users
   before_validation :validate_stock
 
-  def self.buy(params)
-    total = params[:price].to_i*params[:quantity].to_i
-    order = Order.create(total:total, user_id:params[:user_id])
-    order_detail = create(price:params[:price], quantity:params[:quantity], 
-                          post_id:params[:post_id], order_id:order.id)
-    order_detail
+  def self.buy(user)
+    puts(user)
+    Cart.where(status: true, user_id: user.id).update_all(status: false)
   end
   def self.post(post_id)
     post = Post.find(post_id)
